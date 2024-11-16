146d27ad8293ba39e00a4859a55465d5d32d2c5a
Allenzo Paul <allenzopaul.jr@gmail.com>
2024-11-16 15:27:01 +0530

    added index.html

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import psutil
-from flask import Flask
-
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    cpu_percent = psutil.cpu_percent()
-    mem_percent = psutil.virtual_memory().percent
-    Message = None
+    return render_template("index.html")
 
-    if cpu_percent > 80 or mem_percent > 80:
-        Message = "High CPU or Memory utilization detected. Please Scale UP!"
-    return f"CPU Utilization: {cpu_percent} and Memory Utilization: {mem_percent}"
+@app.route("/metrics")
+def metrics():
+    cpu_metric = psutil.cpu_percent()
+    mem_metric = psutil.virtual_memory().percent
+    message = None
+    if cpu_metric > 80 or mem_metric > 80:
+        message = "High CPU or Memory Detected, scale up!!!"
+    return jsonify(cpu_metric=cpu_metric, mem_metric=mem_metric, message=message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
